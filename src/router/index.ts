6e302b1ee59b336fa7abe2fb8b23024b293ae56b
Danithza <158111921+Danithza@users.noreply.github.com>
2025-10-06 18:03:16 -0500

    ruta de contacto

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,6 +3,7 @@ import DefaultLayout from '@/layouts/DefaultLayout.vue'
 import InicioView from '../views/InicioView.vue'
 import ProductosView from '@/views/ProductosView.vue'
 import DetalleProductoView from '../views/DetalleProductoVie.vue'
+import ContactoView from '@/views/ContactoView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,7 @@ const router = createRouter({
         { path: '', name: 'home', component: InicioView },
         { path: 'productos', name: 'productos', component: ProductosView },
         { path: 'producto/:id', name: 'detalle', component: DetalleProductoView },
+        { path: 'contacto', name: 'contacto', component: ContactoView },
       ],
     },
   ],
