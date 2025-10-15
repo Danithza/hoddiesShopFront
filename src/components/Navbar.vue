3769867c1d490b3dfa2d0fb72480c472a6a0bd0d
Danithza <158111921+Danithza@users.noreply.github.com>
2025-10-15 10:26:52 -0500

    navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,18 +1,48 @@
 <template>
   <v-app-bar color="primary" :elevation="2">
     <v-container class="d-flex align-center">
+      <!-- Botón menú lateral -->
       <v-btn icon color="white" @click="drawer = !drawer">
         <v-icon>mdi-menu</v-icon>
       </v-btn>
 
-      <v-app-bar-title class="text-h5 font-weight-bold text-white">HoddieShop</v-app-bar-title>
+      <!-- Título -->
+      <v-app-bar-title class="text-h5 font-weight-bold text-white">
+        HoddieShop
+      </v-app-bar-title>
 
       <v-spacer></v-spacer>
 
-      <v-btn v-for="item in navItems" :key="item.title" :to="item.to" variant="text" color="white">
+      <!-- Navegación principal -->
+      <v-btn
+        v-for="item in navItems"
+        :key="item.title"
+        :to="item.to"
+        variant="text"
+        color="white"
+      >
         {{ item.title }}
       </v-btn>
 
+      <!-- Menú de usuario -->
+      <v-menu v-model="userMenu" location="bottom end" transition="scale-transition">
+        <template #activator="{ props }">
+          <v-btn icon color="white" v-bind="props">
+            <v-icon>mdi-account</v-icon>
+          </v-btn>
+        </template>
+
+        <v-list>
+          <v-list-item :to="'/login'" link>
+            <v-list-item-title>Iniciar Sesión</v-list-item-title>
+          </v-list-item>
+          <v-list-item :to="'/registro'" link>
+            <v-list-item-title>Registrarse</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+
+      <!-- Carrito -->
       <v-btn icon color="white" class="ml-2" @click="cartVisible = true">
         <v-badge :content="cartStore.totalItems" color="secondary" overlap>
           <v-icon>mdi-cart</v-icon>
@@ -23,10 +53,16 @@
     </v-container>
   </v-app-bar>
 
+  <!-- Drawer lateral -->
   <v-navigation-drawer v-model="drawer" temporary>
     <v-list>
       <v-list-subheader>Categorías</v-list-subheader>
-      <v-list-item v-for="cat in categorias" :key="cat.title" :to="cat.to" link>
+      <v-list-item
+        v-for="cat in categorias"
+        :key="cat.title"
+        :to="cat.to"
+        link
+      >
         <v-list-item-title>{{ cat.title }}</v-list-item-title>
       </v-list-item>
     </v-list>
@@ -40,6 +76,7 @@ import CartDialog from './CartDialog.vue'
 
 const drawer = ref(false)
 const cartVisible = ref(false)
+const userMenu = ref(false)
 const cartStore = useCartStore()
 
 const navItems = [
